d50dfc55d771604de8c02aff3f1fccc64ab290b0
dave <daveg7lee@gmail.com>
2020-12-10 16:05:18 +0900

    isLoggedIn part One

--- App.js
+++ App.js
@@ -5,14 +5,40 @@ import { Asset } from "expo-asset";
 import { Text, View } from "react-native";
 import styled from "styled-components/native";
 import { Ionicons } from "@expo/vector-icons";
+import AsyncStorage from "@react-native-community/async-storage";
+import ApolloClient from "apollo-boost";
+import { ThemeProvider } from "styled-components";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { persistCache } from "apollo-cache-persist";
+import { ApolloProvider } from "react-apollo-hooks";
+import apolloClientOptions from "./apollo";
+import styles from "./styles";
 
 export default function App() {
   const [loaded, setLoaded] = useState(false);
+  const [client, setClient] = useState(null);
+  const [isLoggedIn, setIsLoggedIn] = useState(null);
   const preLoad = async () => {
     try {
       await Font.loadAsync(Ionicons.font);
       await Asset.loadAsync([require("./assets/icon.png")]);
+      const cache = new InMemoryCache();
+      await persistCache({
+        cache,
+        storage: AsyncStorage,
+      });
+      const client = new ApolloClient({
+        cache,
+        ...apolloClientOptions,
+      });
+      const isLoggedIn = await AsyncStorage.getItem("isLoggedIn");
+      if (isLoggedIn === null || isLoggedIn === false) {
+        setIsLoggedIn(false);
+      } else {
+        setIsLoggedIn(true);
+      }
       setLoaded(true);
+      setClient(client);
     } catch (e) {
       console.log(e);
     }
@@ -20,10 +46,18 @@ export default function App() {
   useEffect(() => {
     preLoad();
   }, []);
-  return loaded ? (
-    <View>
-      <Text>Open up App.js to Starting working</Text>
-    </View>
+  return loaded && client && isLoggedIn !== null ? (
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={styles}>
+        <View>
+          {isLoggedIn === true ? (
+            <Text>I'm Log In</Text>
+          ) : (
+            <Text>I'm not Log In</Text>
+          )}
+        </View>
+      </ThemeProvider>
+    </ApolloProvider>
   ) : (
     <AppLoading />
   );
