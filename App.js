abaf26e052575d151cc7e38d32e5036d2b96199f
dave <daveg7lee@gmail.com>
2020-12-13 09:57:31 +0900

    AuthContext part Two

--- App.js
+++ App.js
@@ -17,6 +17,7 @@ import NavController from "./Components/NavController";
 export default function App() {
   const [loaded, setLoaded] = useState(false);
   const [client, setClient] = useState(null);
+  const [isLoggedIn, setIsLoggedIn] = useState(null);
   const preLoad = async () => {
     try {
       await Font.loadAsync(Ionicons.font);
@@ -30,6 +31,12 @@ export default function App() {
         cache,
         ...apolloClientOptions,
       });
+      const isLoggedIn = await AsyncStorage.getItem("isLoggedIn");
+      if (!isLoggedIn || isLoggedIn === "false") {
+        setIsLoggedIn(false);
+      } else {
+        setIsLoggedIn(true);
+      }
       setLoaded(true);
       setClient(client);
     } catch (e) {
@@ -40,10 +47,10 @@ export default function App() {
     preLoad();
   }, []);
 
-  return loaded && client ? (
+  return loaded && client && isLoggedIn !== null ? (
     <ApolloProvider client={client}>
       <ThemeProvider theme={styles}>
-        <AuthProvider>
+        <AuthProvider isLoggedIn={isLoggedIn}>
           <NavController />
         </AuthProvider>
       </ThemeProvider>
